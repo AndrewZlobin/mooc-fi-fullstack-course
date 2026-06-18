73b9509e1aa1d5cc0f0eb791619813f2fecaa870
AndrewZlobin <andrewzlobin1992@gmail.com>
2026-06-18 09:41:47 +0200

    Part 1 - Introduction to React: - 1.3: modify variable definitions to objects + app refactoring;

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,11 +1,17 @@
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     const Header = (props) => {
         return (
@@ -17,16 +23,16 @@ const App = () => {
 
     const Part = (props) => {
         return (
-            <p>{props.part} {props.exercises}</p>
+            <p>{props.part.name} {props.part.exercises}</p>
         )
     }
 
     const Content = () => {
         return (
             <div>
-                <Part part={part1} exercises={exercises1} />
-                <Part part={part2} exercises={exercises2} />
-                <Part part={part3} exercises={exercises3} />
+                <Part part={part1} />
+                <Part part={part2} />
+                <Part part={part3} />
             </div>
         )
     }
@@ -34,7 +40,7 @@ const App = () => {
     const Total = () => {
         return (
             <div>
-                <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+                <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
             </div>
         )
     }
