e40488e8e2cf28f5c11929c5c59b38cc8f3cd1fc
AndrewZlobin <andrewzlobin1992@gmail.com>
2026-06-18 09:55:26 +0200

    Part 1 - Introduction to React: - 1.5: further modifications for variables + app refactoring;

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,37 +1,32 @@
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     const Header = (props) => {
         return (
             <div>
-                <h1>{props.course}</h1>
+                <h1>{props.course.name}</h1>
             </div>
         )
     }
 
-    const Part = (props) => {
-        const {name, exercises} = props.part;
-        return (
-            <p>{name} {exercises}</p>
-        )
-    }
-
     const Content = (props) => {
-        const {parts} = props;
+        const {parts} = props.course;
 
         return (
             <div>
@@ -40,8 +35,16 @@ const App = () => {
         )
     }
 
-    const Total = () => {
-        const sum = parts.reduce((sum, part) => sum + part.exercises, 0);
+    const Part = (props) => {
+        const {name, exercises} = props.part;
+
+        return (
+            <p>{name} {exercises}</p>
+        )
+    }
+
+    const Total = (props) => {
+        const sum = props.course.parts.reduce((sum, part) => sum + part.exercises, 0);
         return (
             <div>
                 <p>Number of exercises {sum}</p>
@@ -52,8 +55,8 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Content course={course} />
+            <Total course={course} />
         </div>
     )
 }
