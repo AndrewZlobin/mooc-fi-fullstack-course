3f9c4f9fe17dbc6370d92e115fe5338d6bbb4878
AndrewZlobin <andrewzlobin1992@gmail.com>
2026-07-03 08:56:32 +0200

    Part 1 - A more complex state, debugging React apps: - refactor application - 'Statistics' is now an extracted component (exercise 1.8 -> unicafe step 3);

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,16 +2,7 @@ import {useState} from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
-    const increaseGoodFeedback = () => setGood(good + 1);
-    const increaseNeutralFeedback = () => setNeutral(neutral + 1);
-    const increaseBadFeedback = () => setBad(bad + 1);
-
+const Statistics = ({good, neutral, bad}) => {
     const totalFeedbacks = () => good + neutral + bad;
     const averageFeedbacks = () => {
         const total = totalFeedbacks();
@@ -30,11 +21,6 @@ const App = () => {
 
     return (
         <div>
-            <h1>give feedback</h1>
-            <Button onClick={increaseGoodFeedback} text={'good'}/>
-            <Button onClick={increaseNeutralFeedback} text={'neutral'}/>
-            <Button onClick={increaseBadFeedback} text={'bad'}/>
-
             <h1>statistics</h1>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
@@ -42,6 +28,28 @@ const App = () => {
             <p>all {totalFeedbacks()}</p>
             <p>average {averageFeedbacks()}</p>
             <p>positive {positivePercentageFeedbacks()} %</p>
+        </div>
+    )
+}
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const increaseGoodFeedback = () => setGood(good + 1);
+    const increaseNeutralFeedback = () => setNeutral(neutral + 1);
+    const increaseBadFeedback = () => setBad(bad + 1);
+
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button onClick={increaseGoodFeedback} text={'good'}/>
+            <Button onClick={increaseNeutralFeedback} text={'neutral'}/>
+            <Button onClick={increaseBadFeedback} text={'bad'}/>
+
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
