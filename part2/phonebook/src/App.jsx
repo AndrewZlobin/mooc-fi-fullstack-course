2336d3ede4d1d6d71f8218a9ce427d326cf7d605
AndrewZlobin <andrewzlobin1992@gmail.com>
2026-07-14 21:29:06 +0200

    Part 2 - Communicating with server: - exercise init + ability to add name to the phonebook (the phonebook, exercise 2.6);

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+    const [newName, setNewName] = useState('')
+
+    const handleNewName = (event) => {
+        setNewName(event.target.value);
+    }
+
+    const addNewPerson = (event) => {
+        event.preventDefault();
+        setPersons([
+            ...persons,
+            {name: newName},
+        ]);
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addNewPerson}>
+                <div>
+                    name: <input onChange={handleNewName}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(person => (
+                <p key={person.name}>{person.name}</p>
+            ))}
+        </div>
+    )
+}
+
+export default App
