583e0f1a89cfbcbd0de03298a604544985fbbdeb
AndrewZlobin <andrewzlobin1992@gmail.com>
2026-08-04 22:43:20 +0200

    Part 2 - Getting data from server: - The Phonebook step 11 (exercise 2.16) - success message on adding new entry/updating number for existing one;

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -1,6 +1,11 @@
 import personsService from "../services/persons.js"
+import {useState} from "react";
+import Notification from "./Notification.jsx";
 
 const PersonForm = ({persons, setPersons, newName, setNewName, newNumber, setNewNumber}) => {
+
+    const [successMessage, setSuccessMessage] = useState(null);
+
     const handleNewName = (event) => {
         setNewName(event.target.value)
     }
@@ -27,6 +32,9 @@ const PersonForm = ({persons, setPersons, newName, setNewName, newNumber, setNew
                     setPersons(
                         persons.map(person => person.name === data.name ? data : person)
                     )
+
+                    setSuccessMessage(`${newName} was changed successfully.`)
+                    setTimeout(() => setSuccessMessage(null), 5000);
                 })
             }
 
@@ -44,20 +52,26 @@ const PersonForm = ({persons, setPersons, newName, setNewName, newNumber, setNew
             ...persons,
             newPerson,
         ])
+
+        setSuccessMessage(`${newName} added to phonebook`)
+        setTimeout(() => setSuccessMessage(null), 5000);
     }
 
     return (
-        <form onSubmit={addNewPerson}>
-            <div>
-                name: <input onChange={handleNewName}/>
-            </div>
-            <div>
-                number: <input onChange={handleNewNumber}/>
-            </div>
-            <div>
-                <button type="submit">add</button>
-            </div>
-        </form>
+        <div>
+            <Notification message={successMessage} />
+            <form onSubmit={addNewPerson}>
+                <div>
+                    name: <input onChange={handleNewName}/>
+                </div>
+                <div>
+                    number: <input onChange={handleNewNumber}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
